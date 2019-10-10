08d7f5cb52c7a0eb1d35c93370d52c5092a3b6a5
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-09 22:27:42 -0400

    formatting code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-/*Tyler Eikenberg****
+/****Tyler Eikenberg****
 ******************************************
 This code will simulate 
 the card game War in the console.
 *******************************************/
 
 
-/*Take mainDeck and randomize the position of all the values within the array
-The shuffleCards function takes an array passed through it and reposositions
-all the values inside to random indexes.
+/*The shuffleCards function takes an array and 
+randomize the position of all the values within it. 
 @Param an array*/
 const shuffleCards = (array) => {
 	let currentIndex = array.length, temporaryValue, randomIndex;
@@ -53,11 +52,13 @@ const setupGame = () => {
 					14,14,14,14];
 
 	
-/*	The following code block 
+	/*	
+	The following code block 
 	delays code for a few seconds 
 	then shuffles the cards
 	using the shuffleCard function 
-	passing the mainDeck as the parameter.*/
+	passing the mainDeck as the parameter.
+	*/
 	console.log("Shuffling cards");
 	setTimeout(function(){
          console.log(".")},1000);
@@ -65,14 +66,18 @@ const setupGame = () => {
          console.log(".")},2000);
 	setTimeout(function(){
          console.log(".")},3000);
+	//Shuffle mainDeck*******************
 	shuffleCards(mainDeck);
 	setTimeout(function(){
          console.log("The cards have been shuffled")},4000);
 
 	
 
-/*	Returning initial game with two randomized hands. 
-	Current phase is draw.*/
+	/*	
+	Returning initial game with two randomized hands;
+	one for each player. 
+	Current phase is draw.
+	*/
 	return {
 		playerOne: mainDeck.slice(0,26),
 		playerTwo: mainDeck.slice(26,53),
@@ -82,7 +87,7 @@ const setupGame = () => {
 
 }
 
-	//Game begins
+//Define variable gameState using setupGame to start a new game.
 let gameState = setupGame();
 
 
@@ -93,13 +98,11 @@ if case is waiting initiate waiting function
 if there is a winner exit loop
 */
 while(!gameState.winner){
-	console.log(gameState);
 	switch(gameState.currentState){
 		case 'drawingCards':
-		console.log("Drawing cards")
+		console.log("Drawing cards");
 		 // do something to draw cards
 		 // gameState = drawCards(gameState);
-		 setTimeout(function(){gameState.currentState = 'waiting'}, 5000);
 		 break;
 	 	case 'waiting':
 		 // show please hit enter
